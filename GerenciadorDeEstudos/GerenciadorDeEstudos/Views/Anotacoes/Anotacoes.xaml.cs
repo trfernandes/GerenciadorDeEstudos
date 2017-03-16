3d024a1bdfd86be2af34bdbab12a9042e77c6df7
Micael Gomes de Souza <micael@metadados.com.br>
2017-03-15 22:47:01 -0300

    Layout

---
+++ GerenciadorDeEstudos/GerenciadorDeEstudos/Views/Anotacoes/Anotacoes.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace GerenciadorDeEstudos.Views.Anotacoes
+{
+    public partial class Anotacoes : ContentPage
+    {
+        public Anotacoes()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GerenciadorDeEstudos/GerenciadorDeEstudos/Views/Início/MenuItems/MasterPageItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeEstudos.Views.Início.MenuItems
+{
+    public class MasterPageItem
+    {
+
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public Type TargetType { get; set; }
+    }
+}
